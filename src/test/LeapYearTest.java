778aadebaf53c32041d7d76d1a8a48069bcae52f
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-19 16:17:06 +0200

    not leap year - red

--- src/test/LeapYearTest.java
+++ src/test/LeapYearTest.java
@@ -8,6 +8,11 @@ public class LeapYearTest {
         assertThat(isLeapYear(2020)).isTrue();
     }
 
+    @Test
+    void notLeapYear() {
+        assertThat(isLeapYear(2100)).isFalse();
+    }
+
     private boolean isLeapYear(int year) {
         if (year % 4 == 0) return true;
         return false;
