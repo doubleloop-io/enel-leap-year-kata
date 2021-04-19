0ae28df50d92f8f4ac23998a4cfaee1dfc1f99dc
Andrea Vallotti <andrea.vallotti@gmail.com>
2021-04-19 16:17:47 +0200

    not leap year - green

--- src/test/LeapYearTest.java
+++ src/test/LeapYearTest.java
@@ -14,6 +14,7 @@ public class LeapYearTest {
     }
 
     private boolean isLeapYear(int year) {
+        if (year % 100 == 0) return false;
         if (year % 4 == 0) return true;
         return false;
     }
